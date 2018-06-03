9a6dbccd0d5e8970ab5a3fa6af70e9ff1e4cc527
Monica Mamdouh <monica.mamdouh@cis.asu.edu.eg>
2018-06-04 00:35:34 +0200

    fist commit

---
+++ app/src/main/java/com/example/monica/esrisample/ItemData.java
@@ -0,0 +1,23 @@
+package com.example.monica.esrisample;
+
+/**
+ * Created by monica on 6/3/2018.
+ */
+public class ItemData {
+
+    private final String text;
+    private final Integer imageId;
+
+    public ItemData(String text, Integer imageId) {
+        this.text = text;
+        this.imageId = imageId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Integer getImageId() {
+        return imageId;
+    }
+}
